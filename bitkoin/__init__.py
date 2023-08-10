e2b4a35289c33bf8eabbe373bce899640737e227
Slut <slut@bimbo.com>
2023-08-10 23:10:57 +0300

    base

---
+++ bitkoin/__init__.py
@@ -0,0 +1,1 @@
+from bitkoin.main import run
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bitkoin',
+    version='0.1',
+    author='slut',
+    packages=find_packages(),
+)
